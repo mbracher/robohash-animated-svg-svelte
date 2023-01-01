f4374dd16768f7eb6d138d8ca1252366cc087ee2
marc <m.bracher@mac.com>
2023-01-01 13:45:03 +0100

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import RoboHash from "../lib/RoboHash.svelte";
+    let robot;
+</script>
+
+<h1>Welcome to RoboHash-Svelte</h1>
+
+<RoboHash bind:this={robot} width={500} height={500} string="VCpL+ttOxnunjiptuA5AB+JjFYcAFOMOVPuVLt/HE2Q="/>
+
+<button on:click={() => robot.start()}>start</button>
