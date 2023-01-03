07f6e37b2e4d03e52707431f9f8aae7357f7e858
marc <m.bracher@mac.com>
2023-01-03 22:28:49 +0100

    hash method compatible with original

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,20 @@
 <script>
-    import RoboHash from "../lib/RoboHash.svelte";
+    import Robohash from "../lib/robohash.svelte";
     let robot1;
     let robot2;
 </script>
 
-<h1>Welcome to RoboHash-SVG-Svelte</h1>
+<h1>Welcome to Robohash-animated-svg-svelte</h1>
 <p>
-<RoboHash bind:this={robot1} width={500} height={500} string="VCpL+ttOxnunjiptuA5AB+JjFYcAFOMOVPuVLt/HE2Q="/>
+<Robohash bind:this={robot1} width={500} height={500} string="VCpL+ttOxnunjiptuA5AB+JjFYcAFOMOVPuVLt/HE2Q="/>
 
-<button on:click={() => robot1.start()}>start</button>
+<!-- enable/disable the right button depending on the state of the animation -->
+<button on:click={() => robot1.start()}>start animation</button>
+<button on:click={() => robot1.stop()}>stop animation</button>
 </p>
-<p>
-<RoboHash bind:this={robot2} width={500} height={500} string="TPHp5v7aZbfmHFL5w/nEM9nToyATqyvsyt/JDPR2dkk="/>
 
-<button on:click={() => robot2.start()}>start</button>
+<p>
+{#each Array(10) as _, i}
+    <Robohash width={200} height={200} string={i.toString()}/>
+{/each}
 </p>
