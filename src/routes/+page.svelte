309f7bbadc9a37a5c08ddabc3a4b3024fe2ed09f
marc <m.bracher@mac.com>
2023-01-08 17:08:59 +0100

    produce same robot on the same hash

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,21 @@
-<script>
-    import Robohash from "../lib/robohash.svelte";
-    let robot1;
-    let robot2;
+<script  lang="ts">
+	import Robohash from "../lib/robohash.svelte";
+
+    let robot1: Robohash;
+
 </script>
 
 <h1>Welcome to Robohash-animated-svg-svelte</h1>
 <p>
-<Robohash bind:this={robot1} width={500} height={500} string="VCpL+ttOxnunjiptuA5AB+JjFYcAFOMOVPuVLt/HE2Q="/>
+<Robohash bind:this={robot1} width={500} height={500} value="VCpL+ttOxnunjiptuA5AB+JjFYcAFOMOVPuVLt/HE2Q="/>
 
-<!-- enable/disable the right button depending on the state of the animation -->
+<!-- TODO: enable/disable the right button depending on the state of the animation -->
 <button on:click={() => robot1.start()}>start animation</button>
 <button on:click={() => robot1.stop()}>stop animation</button>
 </p>
 
 <p>
 {#each Array(10) as _, i}
-    <Robohash width={200} height={200} string={i.toString()}/>
+    <Robohash width={200} height={200} index={i}/>
 {/each}
 </p>
